3ef6e00c11d95e9fbbf14b7d2a0ed44e5f072687
coder-Rishi05 <rawatrishi1121@gmail.com>
2026-02-13 17:49:47 +0530

    added cors error fixed

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ import express from "express";
 import authRouter from "./src/routes/auth.routes.js";
 import urlRouter from "./src/routes/url.routes.js";
 import adminRouter from "./src/routes/admin.routes.js";
+import cors from "cors";
 
 export const app = express();
-app.use(express.json());
 
+// app.options("*", cors());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.use(express.json());
 
 app.use("/api/auth", authRouter);
 
