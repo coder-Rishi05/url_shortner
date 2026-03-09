6cf1b56577be0d3f8889ef0bfd049f08e4d507ab
coder-Rishi05 <rawatrishi1121@gmail.com>
2026-03-09 19:00:43 +0530

    rem limit

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ import authRouter from "./src/routes/auth.routes.js";
 import urlRouter from "./src/routes/url.routes.js";
 import adminRouter from "./src/routes/admin.routes.js";
 import cors from "cors";
-import paymentRoutes from "./src/routes/payment.routes.js";
-import webhookRoutes from "./src/routes/webhook.routes.js";
+// import paymentRoutes from "./src/routes/payment.routes.js";
+// import webhookRoutes from "./src/routes/webhook.routes.js";
 import cookieParser from "cookie-parser";
 import { redirectUrl } from "./src/controllers/url.controller.js";
 import passport from "passport";
@@ -31,7 +31,7 @@ app.use(passport.session());
 app.options(/.*/, cors(corsOptions));
 app.use(cors(corsOptions));
 
-app.use("/api/webhooks", webhookRoutes);
+// app.use("/api/webhooks", webhookRoutes);
 app.use(express.json());
 app.use(cookieParser());
 
@@ -39,4 +39,4 @@ app.use("/api/auth", authRouter);
 app.use("/api/urls", urlRouter);
 app.get("/:shortCode", redirectUrl);
 app.use("/api/admin", adminRouter);
-app.use("/api/payments", paymentRoutes);
+// app.use("/api/payments", paymentRoutes);
