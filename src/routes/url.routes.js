afe69efb3341cfb8fbc2af461460dbee1328db49
coder-Rishi05 <rawatrishi1121@gmail.com>
2026-03-09 18:34:53 +0530

    rem limit

--- src/routes/url.routes.js
+++ src/routes/url.routes.js
@@ -11,16 +11,16 @@ import { protect } from "../middlewares/auth.middleware.js";
 import rateLimit from "express-rate-limit";
 
 // 10 req in 1 minutes
-const limiter = rateLimit({
-  windowMs: 1000 * 60 * 1,
-  max: 10,
-  message: "Too many request limit reached",
-});
+// const limiter = rateLimit({
+//   windowMs: 1000 * 60 * 1,
+//   max: 10,
+//   message: "Too many request limit reached",
+// });
 
 const router = Router();
 
 // create short url (protected)
-router.post("/", protect, limiter, createUrl);
+router.post("/", protect,  createUrl);
 
 // get logged-in user's urls (protected)
 router.get("/", protect, getUserUrls);
