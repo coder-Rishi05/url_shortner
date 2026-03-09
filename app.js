87a24f6b81d2ffa010ad7e557b54bace37489561
coder-Rishi05 <rawatrishi1121@gmail.com>
2026-03-09 13:41:27 +0530

    change url

--- app.js
+++ app.js
@@ -9,51 +9,34 @@ import cookieParser from "cookie-parser";
 import { redirectUrl } from "./src/controllers/url.controller.js";
 import passport from "passport";
 import session from "express-session";
-import helmet from "helmet";
 
 export const app = express();
 
-// session setup
+const corsOptions = {
+  origin: "https://url-frontend-fhzz.onrender.com",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-app.use(
-  session({
-    secret: "mysecret",
-    resave: false,
-    saveUninitialized: true,
-  }),
-);
-
-// starting passport
+app.use(session({
+  secret: "mysecret",
+  resave: false,
+  saveUninitialized: true,
+}));
 
 app.use(passport.initialize());
-// creating session for passport
 app.use(passport.session());
-app.options("/{*path}", cors());
-app.use(
-  cors({
-    origin: "https://url-frontend-fhzz.onrender.com",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
-// app.use(
-//   helmet({
-//     crossOriginResourcePolicy: false,
-//     crossOriginOpenerPolicy: false,
-//     crossOriginEmbedderPolicy: false,
-//     contentSecurityPolicy: false,
-//   }),
-// );
+
+app.options("/{*path}", cors(corsOptions));
+app.use(cors(corsOptions));
+
 app.use("/api/webhooks", webhookRoutes);
-
 app.use(express.json());
-
 app.use(cookieParser());
-app.use("/api/auth", authRouter);
 
+app.use("/api/auth", authRouter);
 app.use("/api/urls", urlRouter);
 app.get("/:shortCode", redirectUrl);
 app.use("/api/admin", adminRouter);
-
 app.use("/api/payments", paymentRoutes);
