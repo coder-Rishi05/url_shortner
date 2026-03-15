adce1337f3006e42efb6d9525b8f409e1d95b90c
coder-Rishi05 <rawatrishi1121@gmail.com>
2026-03-15 16:40:58 +0530

    fixed logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -86,7 +86,11 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    res.clearCookie("token");
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true, // ✅ uncomment karo — HTTPS pe zaroori hai
+      sameSite: "none",
+    });
     res.status(200).json({
       message: "User logged out successfully",
     });
